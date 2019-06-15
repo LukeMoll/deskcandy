9d018d6e86d357f52638aa83a9d94846219c83fa
Luke Moll <luke.moll99@gmail.com>
2019-06-15 17:15:21 +0100

    Added OLED timeout to dma_oled

--- dma_oled/dma_oled.ino
+++ dma_oled/dma_oled.ino
@@ -1,7 +1,10 @@
 #include <Arduino.h>
 #include <NeoPixelBus.h>
+
+#define _LED D9 // D9 ignored for ESP8266 - connect to "RX"
+
 #define NUM_LEDS 35
-NeoPixelBus<NeoGrbFeature, Neo800KbpsMethod> leds(NUM_LEDS, D9); // D9 ignored for ESP8266 - connect to "RX"
+NeoPixelBus<NeoGrbFeature, Neo800KbpsMethod> leds(NUM_LEDS, _LED); 
 
 #include <U8x8lib.h>
 
@@ -15,6 +18,8 @@ NeoPixelBus<NeoGrbFeature, Neo800KbpsMethod> leds(NUM_LEDS, D9); // D9 ignored f
 // for the older looking one
 U8X8_SSD1305_128X64_ADAFRUIT_4W_SW_SPI u8x8(_SCK, _MOSI, _CS, _DC, _RST);
 
+#define _BUTTON D2
+
 #define HUE_WIDTH 0.4f
 #define TICKLEN_MS 25
 #define SEQLEN_MS 10000 
@@ -27,9 +32,13 @@ void setup() {
   base_hues(NUM_LEDS, HUE_WIDTH);
   leds.Show();
 
+  pinMode(_BUTTON, INPUT);
+
   u8x8.begin();
+
+  // all of this should probably go in a fn
   u8x8.setPowerSave(0);
-  u8x8.setFont(u8x8_font_chroma48medium8_r); // works?
+  u8x8.setFont(u8x8_font_chroma48medium8_r); // works? needs latest u8g2
   char buf[16];
   snprintf(buf,16,"%3d LEDs", NUM_LEDS);
   u8x8.drawString(0,0,buf);
@@ -64,8 +73,20 @@ void progress(int16_t num_ticks) {
     leds.SetPixelColor(i, current);
   }
 }
+
+bool buttonPressed = false;
+unsigned long screen_millis = millis();
+
 void loop() {
   progress(SEQLEN_MS / TICKLEN_MS);
   leds.Show();
   delay(TICKLEN_MS);
+  
+  if(digitalRead(_BUTTON) == LOW && !buttonPressed) {
+    u8x8.setPowerSave(0);
+    screen_millis = millis();
+  }
+  if(millis() - screen_millis > 60000) { // screen has been on for a minute
+    u8x8.setPowerSave(1);
+  }
 }
